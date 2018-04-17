7962ceebfdd62b6fee4df98fe7546a78facc9a46
Shantanu Sharma <sharma@horangi.com>
2018-04-17 18:59:54 +0800

    Update SettingFragmentActivity.java

--- src/main/java/com/bustripper/bustripper/UserInterface/SettingFragmentActivity.java
+++ src/main/java/com/bustripper/bustripper/UserInterface/SettingFragmentActivity.java
@@ -1,183 +1,125 @@
 package com.bustripper.bustripper.UserInterface;
 
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ExpandableListView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bustripper.bustripper.Entity.BusService;
 import com.bustripper.bustripper.R;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
  * {@link SettingFragmentActivity.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link SettingFragmentActivity#newInstance} factory method to
+ * Use the {@link SettingFragmentActivity newInstance} factory method to
  * create an instance of this fragment.
  */
 public class SettingFragmentActivity extends Fragment {
 
     // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-    // For ExpandableListView
-    ExpandableListView lv;
-    private String[] groups;
-    private String[][] children;
-
-    private OnFragmentInteractionListener mListener;
+    private View rootView;
+    EditText oriPass;
+    EditText newPass;
+    EditText confirmPass;
+    EditText arrivalTime;
+    EditText alightDist;
 
     public SettingFragmentActivity() {
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        groups = new String[] { "Change Password", "Change Notification"};
-
-        children = new String [][] {
-                { "Current Password", "New Password", "Retype New Password" },
-                { "Arriving Bus Alert", "Reminder to Alight" }
-        };
-    }
-
-    @Nullable
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_setting, container, false);
+        super.onCreate(savedInstanceState);
+        this.rootView = inflater.inflate(R.layout.fragment_setting, container, false);
+
+        // Find a reference to the password section in the layout
+        oriPass = (EditText)rootView.findViewById(R.id.setting_original_pass_edit);
+        newPass = (EditText)rootView.findViewById(R.id.setting_new_pass_edit);
+        confirmPass = (EditText)rootView.findViewById(R.id.setting_confirm_pass_edit);
+
+        Button passButton = (Button) rootView.findViewById(R.id.setting_save_button_pass);
+
+        passButton.setOnClickListener(new View.OnClickListener() {
+                                             @Override public void onClick(View view){
+                                                 Toast.makeText(getActivity(), "Password Saved",
+                                                         Toast.LENGTH_LONG).show();
+                                             }
+                                         }
+        );
+
+        // Find a reference to the notification section in the layout
+        arrivalTime = (EditText)rootView.findViewById(R.id.setting_arrival_edit);
+        Switch arrivalSwitch = (Switch) rootView.findViewById(R.id.setting_arrival_switch);
+        alightDist = (EditText)rootView.findViewById(R.id.setting_alight_edit);
+        Switch alightSwitch = (Switch) rootView.findViewById(R.id.setting_alight_switch);
+
+        Button notiButton = (Button) rootView.findViewById(R.id.setting_save_button_noti);
+            notiButton.setOnClickListener(new View.OnClickListener() {
+                                              @Override
+                                              public void onClick(View view) {
+                                                  if (verifyArrival(arrivalTime.getText().toString()) && verifyAlight(alightDist.getText().toString())) {
+                                                      Toast.makeText(getActivity(), "Notification Settings Saved",
+                                                              Toast.LENGTH_LONG).show();
+                                                  } else {
+                                                      Toast.makeText(getActivity(), "Input Out of Range!",
+                                                              Toast.LENGTH_LONG).show();
+                                                  }
+                                              }
+                                          }
+            );
+
+
+        return rootView;
     }
 
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        lv = view.findViewById(R.id.expListView);
-        lv.setAdapter(new ExpandableListAdapter(groups, children));
-        lv.setGroupIndicator(null);
-
-    }
-
-    public class ExpandableListAdapter extends BaseExpandableListAdapter {
-
-        private final LayoutInflater inf;
-        private String[] groups;
-        private String[][] children;
-
-        public ExpandableListAdapter(String[] groups, String[][] children) {
-            this.groups = groups;
-            this.children = children;
-            inf = LayoutInflater.from(getActivity());
+    private boolean verifyArrival(String arrivalTime){
+        if (arrivalTime.isEmpty()){
+            return false;
         }
 
-        @Override
-        public Object getChild(int groupPosition, int childPosition) {
-            return children[groupPosition][childPosition];
+        double time = Double.valueOf(arrivalTime);
+        if (time < 1 || time > 15){
+            return false;
         }
-
-        @Override
-        public long getChildId(int groupPosition, int childPosition) {
-            return childPosition;
-        }
-
-        @Override
-        public int getChildrenCount(int groupPosition) {
-            return children[groupPosition].length;
-        }
-
-        @Override
-        public View getChildView(int groupPosition, final int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
-
-            ViewHolder holder;
-            if (convertView == null) {
-                convertView = inf.inflate(R.layout.setting_list_item, parent, false);
-                holder = new ViewHolder();
-                if (groupPosition==0) {
-                    holder.text = convertView.findViewById(R.id.lblListItem1);
-                }
-                else if (groupPosition==1) {
-                    holder.text = convertView.findViewById(R.id.lblListItem2);
-
-                }
-                convertView.setTag(holder);
-            } else {
-                holder = (ViewHolder) convertView.getTag();
-            }
-
-            holder.text.setText(getChild(groupPosition, childPosition).toString());
-
-            return convertView;
-        }
-
-        @Override
-        public Object getGroup(int groupPosition) {
-            return groups[groupPosition];
-        }
-
-        @Override
-        public long getGroupId(int groupPosition) {
-            return groupPosition;
-        }
-
-        @Override
-        public int getGroupCount() {
-            return groups.length;
-        }
-
-        @Override
-        public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
-            ViewHolder holder;
-
-            if (convertView == null) {
-                convertView = inf.inflate(R.layout.setting_list_group, parent, false);
-
-                holder = new ViewHolder();
-                holder.text = convertView.findViewById(R.id.lblListHeader);
-                convertView.setTag(holder);
-            } else {
-                holder = (ViewHolder) convertView.getTag();
-            }
-
-            holder.text.setText(getGroup(groupPosition).toString());
-
-            return convertView;
-        }
-
-        @Override
-        public boolean hasStableIds() {
+        else{
             return true;
         }
+    }
 
+    private boolean verifyAlight(String alightDist){
+        if (alightDist.isEmpty()){
+            return false;
+        }
 
-        @Override
-        public boolean isChildSelectable(int groupPosition, int childPosition) {
+        double dist = Double.valueOf(alightDist);
+        if (dist < 100 || dist > 1000){
+            return false;
+        }
+        else{
             return true;
         }
-
-        private class ViewHolder {
-            TextView text;
-        }
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
 
     /**
      * This interface must be implemented by activities that contain this
@@ -194,4 +136,3 @@ public class SettingFragmentActivity extends Fragment {
         void onFragmentInteraction(Uri uri);
     }
 }
-
