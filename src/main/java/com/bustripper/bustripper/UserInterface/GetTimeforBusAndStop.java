87491531abccd6a79e41759b338b8f9a5fe14fe6
Shantanu Sharma <sharma@horangi.com>
2018-04-17 19:03:29 +0800

    Able to handle API request to display stops according to bus selected

--- src/main/java/com/bustripper/bustripper/UserInterface/GetTimeforBusAndStop.java
+++ src/main/java/com/bustripper/bustripper/UserInterface/GetTimeforBusAndStop.java
@@ -1,20 +1,16 @@
 package com.bustripper.bustripper.UserInterface;
-
-
 import android.content.Context;
-import android.os.AsyncTask;
-import android.util.Log;
 
-import org.json.JSONArray;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import android.os.StrictMode;
+import android.util.Log;
+import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.Calendar;
 
 
@@ -22,16 +18,20 @@ public class GetTimeforBusAndStop {
 
     private final String busNo, stopNo;
     private Context context;
+    static String stop_name;
 
-    public GetTimeforBusAndStop(String busNo, String stopNo) {
+    public GetTimeforBusAndStop(String stopNo, String busNo,String stopName) {
         super();
         this.busNo = busNo;
         this.stopNo = stopNo;
-        this.main();
+        stop_name=stopName;
+        //this.main();
     }
 
     public BusTimes main() {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
 
+        StrictMode.setThreadPolicy(policy);
         // These two need to be declared outside the try/catch
         // so that they can be closed in the finally block.
         HttpURLConnection urlConnection = null;
@@ -42,12 +42,14 @@ public class GetTimeforBusAndStop {
 
         try {
 
-            URL url = new URL("http://datamall2.mytransport.sg/ltaodataservice/BusArrival?BusStopID=" + this.stopNo + "&ServiceNo=" + this.busNo);
+            URL url = new URL("http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2?BusStopCode=" + this.stopNo +"&ServiceNo=" + this.busNo);
 
             // Create the request and open the connection
             urlConnection = (HttpURLConnection) url.openConnection();
+//            urlConnection.setReadTimeout(10000 /* milliseconds */);
+//            urlConnection.setConnectTimeout(15000 /* milliseconds */);
             urlConnection.setRequestMethod("GET");
-            urlConnection.setRequestProperty("AccountKey", "QOuhOOUltXtFKtlHrRpD8A==");
+            urlConnection.setRequestProperty("AccountKey","QOuhOOUltXtFKtlHrRpD8A==");
             urlConnection.setRequestProperty("UniqueUserID", "5cfedab3-e6f2-4ea7-b016-2e658ae60ca8");
             urlConnection.setRequestProperty("accept", "application/json");
             urlConnection.connect();
@@ -70,10 +72,7 @@ public class GetTimeforBusAndStop {
                 BusTimesJsonStr = null;
             }
             BusTimesJsonStr = buffer.toString();
-        } catch (
-                IOException e)
-
-        {
+        } catch (Exception e){
             Log.e("PlaceholderFragment", "Error ", e);
             BusTimesJsonStr = null;
         } finally {
@@ -102,7 +101,7 @@ public class GetTimeforBusAndStop {
     }
     public BusTimes getBusTimes(JSONObject myJSON, int pos){
         BusTimes bt;
-        String no = "";
+        String no = stop_name;
         int t1=-1,t2=-1, t3=-1;
         try{
             JSONObject bus = myJSON.getJSONArray("Services").getJSONObject(pos);
@@ -110,21 +109,21 @@ public class GetTimeforBusAndStop {
 
             String time1 = bus.getJSONObject("NextBus").getString("EstimatedArrival");
             if(time1.equals("")){
-                bt = new BusTimes(no, -1, -1, -1);
+                bt = new BusTimes(stop_name, -1, -1, -1);
                 return  bt;
             }
             t1 = getTimeFromString(time1);
 
             String time2 = bus.getJSONObject("NextBus2").getString("EstimatedArrival");
             if(time2.equals("")){
-                bt = new BusTimes(no, t1 , -1, -1);
+                bt = new BusTimes(stop_name, t1 , -1, -1);
                 return bt;
             }
             t2 = getTimeFromString(time2);
 
             String time3 = bus.getJSONObject("NextBus3").getString("EstimatedArrival");
             if(time3.equals("")){
-                bt = new BusTimes(no, t1, t2, -1);
+                bt = new BusTimes(stop_name, t1, t2, -1);
                 return bt;
             }
             t3 = getTimeFromString(time3);
@@ -132,19 +131,16 @@ public class GetTimeforBusAndStop {
         }catch (Exception e){
             e.printStackTrace();
         }
-        return new BusTimes(no, t1, t2, t3);
+        return new BusTimes(stop_name, t1, t2, t3);
     }
 
     public int getTimeFromString(String time){
         Calendar cal = Calendar.getInstance();
-        int hour = Integer.parseInt(time.substring(11,13))-cal.get(Calendar.HOUR_OF_DAY);
-        int offset = hour>0?hour*60:0;
-        int minute = Integer.parseInt(time.substring(14,16))-cal.get(Calendar.MINUTE)+offset;
+        //int hour = Integer.parseInt(time.substring(11,13))-cal.get(Calendar.HOUR_OF_DAY);
+        //int offset = hour>0?hour*60:0;
+        int minute = Integer.parseInt(time.substring(14,16))-cal.get(Calendar.MINUTE);
         int second = Integer.parseInt(time.substring(17,19))-cal.get(Calendar.SECOND);
         if(second<0){minute--;}
-        return  minute/60;
+        return  minute;
     }
-
 }
-
-
